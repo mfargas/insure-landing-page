050ac123ce60630fb1b300e8730cf7fdec1a56ab
Marjoree <65003463+mfargas@users.noreply.github.com>
2021-08-30 00:14:43 -0700

    Update styling and rearrange asset folder

--- app/js/index.js
+++ app/js/index.js
@@ -1,6 +1,20 @@
+const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+const height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+const mediaQueryMobile = window.matchMedia('(max-width: 425px)');
+
+
 const burger = document.querySelector(".burger");
 const nav = document.querySelector(".menu");
 
-function toggleMenu(){
-
+function handleScreenResize(e) {
+    if (e.matches) {
+        burger.addEventListener('click', () => {
+            nav.classList.toggle("menuToggle");
+        });
+    }
 }
+
+mediaQueryMobile.addListener(handleScreenResize)
+
+handleScreenResize(mediaQueryMobile);
